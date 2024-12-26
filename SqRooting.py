92fb530b73f21f3223b4f69f782c7f4cdeedb6ba
nand32541works <nandakishorejana780@gmail.com>
2024-12-26 13:32:18 +0530

    Create SqRooting.py

---
+++ SqRooting.py
@@ -0,0 +1,11 @@
+# squaring and cubing and rooting of numbers
+
+N = int(input("Enter the number of your choice: "))
+Y = int(input("Enter the root number: "))
+R = (N)**Y
+print(R ,"is the value.")
+
+M = int(input("Enter the number of your choice: "))
+X = float(input("Enter the root number: "))
+U = (M)**(1/Y)
+print(U, "is the value.")
