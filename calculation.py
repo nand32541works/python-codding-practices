a539e73ab55171bc91701e6635f6aca803447194
nand32541works <nandakishorejana780@gmail.com>
2024-12-28 20:08:57 +0530

    Create calculation.py

---
+++ calculation.py
@@ -0,0 +1,12 @@
+#addition, substraction and multiplication of two numbers
+
+N = int(input("Enter the first number: "))
+M = int(input("Enter the second number: "))
+C = N + M
+print(C,"is the result.")
+D = N - M
+print(D,"is the result.")
+E = N * M
+print(E,"is the result.")
+F = N / M
+print(F,"is the result.")
